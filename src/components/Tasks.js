16bbd48c2bf607bfc318088583140addb250111e
Ray Kudjie <rkudjie@gmail.com>
2020-01-27 21:24:23 +0000

    Display Tasks

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,27 +1,44 @@
-import React from 'react'
-import { Checkbox } from '../components/Checkbox'
-import { useTasks } from '../hooks'
+import React, { useState, useEffect } from "react";
+import { Checkbox } from "../components/Checkbox";
+import { useTasks } from "../hooks";
+import { collatedTasks } from "../constants";
+import { getTitle, getCollatedTitle, collatedTasksExist } from "../helpers";
+import { useSelectedProjectValue, useProjectsValue } from "../context";
 
-export const Tasks = ()=>{
-	const { tasks } = useTasks('1');
+export const Tasks = () => {
+	const { selectedProject } = useSelectedProjectValue();
+	const { tasks } = useTasks(selectedProject);
+	const { projects } = useProjectsValue();
 
-	console.log(tasks)
-	let projectName = '';
+	console.log(tasks);
+	let projectName = "";
+
+	if (projects && selectedProject && !collatedTasksExist(selectedProject)) {
+		projectName = getTitle(projects, selectedProject).name;
+		console.log("projectname 1", projectName);
+	}
+
+	if (collatedTasksExist(selectedProject) && selectedProject) {
+		projectName = getCollatedTitle(collatedTasks, selectedProject).name;
+		console.log("projectname 2", projectName);
+	}
+
+	useEffect(() => {
+		document.title = `${projectName} : Todoist`;
+	})
 
 	return (
 		<div className="tasks" data-testid="tasks">
-			<h2 data-testid =" project-name">{projectName}</h2>
-		
-			<ul className="tasks__list">
-				{tasks.map(task=>(
-					<li key={`${task.id}`}>
-						<Checkbox id={task.id}/>
-						<span>{task.task}</span>
+			<h2 data-testid=" project-name">{projectName}</h2>
 
+			<ul className="tasks__list">
+				{tasks.map(task => (
+					<li key={`${task.id}`}>
+						<Checkbox id={task.id} />
+						<span>{task.task}</span>
 					</li>
 				))}
 			</ul>
 		</div>
-
-	)
-}
+	);
+};
--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,82 +1,79 @@
-import { useState, useEffect } from "react";
-import { firebase } from "../firebase";
-import { collatedTasksExist } from "../helpers";
-import moment from "moment";
-import { setState } from "expect/build/jestMatchersObject";
+/* eslint-disable no-nested-ternary */
+import { useState, useEffect } from 'react';
+import moment from 'moment';
+import { firebase } from '../firebase';
+import { collatedTasksExist } from '../helpers';
 
 export const useTasks = selectedProject => {
-	const [tasks, setTasks] = useState([]);
-	const [archivedTasks, setArchivedTasks] = useState([]);
+  const [tasks, setTasks] = useState([]);
+  const [archivedTasks, setArchivedTasks] = useState([]);
 
-	useEffect(() => {
-		let unsubscribe = firebase
-			.firestore()
-			.collection("tasks")
-			.where("userId", "==", "e6o3v6R22X9WkrgdI50");
+  useEffect(() => {
+    let unsubscribe = firebase
+      .firestore()
+      .collection('tasks')
+      .where('userId', '==', 'e6o3v6R22X9WkrgdI50');
 
-		unsubscribe =
-			selectedProject && !collatedTasksExist(selectedProject)
-				? (unsubscribe = unsubscribe.where("projectId", "==", selectedProject))
-				: selectedProject === "TODAY"
-				? (unsubscribe = unsubscribe.where(
-						"date",
-						"==",
-						moment().format("DD/MM/YYYY")
-				  ))
-				: selectedProject === "INBOX" || selectedProject === 0
-				? (unsubscribe = unsubscribe.where("date", "==", ""))
-				: unsubscribe;
+    unsubscribe =
+      selectedProject && !collatedTasksExist(selectedProject)
+        ? (unsubscribe = unsubscribe.where('projectId', '==', selectedProject))
+        : selectedProject === 'TODAY'
+        ? (unsubscribe = unsubscribe.where(
+            'date',
+            '==',
+            moment().format('DD/MM/YYYY')
+          ))
+        : selectedProject === 'INBOX' || selectedProject === 0
+        ? (unsubscribe = unsubscribe.where('date', '==', ''))
+        : unsubscribe;
 
-		unsubscribe = unsubscribe.onSnapshot(snapshot => {
-			const newTasks = snapshot.docs.map(task => ({
-				id: task.id,
-				...task.data()
-			}));
+    unsubscribe = unsubscribe.onSnapshot(snapshot => {
+      const newTasks = snapshot.docs.map(task => ({
+        id: task.id,
+        ...task.data(),
+      }));
 
-			setTasks(
-				selectedProject === "NEXT_7"
-					? newTasks.filter(
-							task =>
-								moment(task.date, "DD-MM-YYYY").diff(moment(), "days") <= 7 &&
-								task.archived !== true
-					  )
-					: newTasks.filter(task => task.archived !== true)
-			);
+      setTasks(
+        selectedProject === 'NEXT_7'
+          ? newTasks.filter(
+              task =>
+                moment(task.date, 'DD-MM-YYYY').diff(moment(), 'days') <= 7 &&
+                task.archived !== true
+            )
+          : newTasks.filter(task => task.archived !== true)
+      );
+      setArchivedTasks(newTasks.filter(task => task.archived !== false));
+    });
 
-			setArchivedTasks(newTasks.filter(task => task.archived !== false));
-		});
+    return () => unsubscribe();
+  }, [selectedProject]);
 
-		return () => unsubscribe();
-	}, [selectedProject]);
-
-	return { tasks, archivedTasks };
+  return { tasks, archivedTasks };
 };
-
-const selectedProject = 1;
-const { tasks, archivedTasks } = useTasks(selectedProject);
 
 export const useProjects = () => {
-	const [projects, setProjects] = setState([]);
+  const [projects, setProjects] = useState([]);
 
-	useEffect(() => {
-		firebase
-			.firestore()
-			.collection("projects")
-			.where("userId", "==", "e6o3v6R22X9WkrgdI50")
-			.orderBy("projectId")
-			.get()
-			.then(snapshot => {
-				const allProjects = snapshot.docs.map(project => ({
-					...project.data(),
-					docId: project.id
-				}));
+  useEffect(() => {
+    firebase
+      .firestore()
+      .collection('projects')
+      .where('userId', '==', 'e6o3v6R22X9WkrgdI50')
+      .orderBy('projectId')
+      .get()
+      .then(snapshot => {
+        const allProjects = snapshot.docs.map(project => ({
+          ...project.data(),
+          docId: project.id,
+        }));
 
-				if (JSON.stringify(allProjects) !== JSON.stringify(projects)) {
-					setProjects(allProjects);
-				}
-			});
-		
-	}, [projects]);
+        if (JSON.stringify(allProjects) !== JSON.stringify(projects)) {
+          setProjects(allProjects);
+        }
+      });
+  }, [projects]);
 
-	return { projects, setProjects };
+  return { projects, setProjects };
 };
+
+
