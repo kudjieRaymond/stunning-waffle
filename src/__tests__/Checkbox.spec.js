8d79ed74fab09b1b0e1cc1b662af60244c3ff667
Ray Kudjie <rkudjie@gmail.com>
2020-02-03 10:31:12 +0000

    Setup test

--- src/__tests__/Checkbox.spec.js
+++ src/__tests__/Checkbox.spec.js
@@ -1,1 +1,50 @@
 import React from 'react';
+import {render, cleanup, fireEvent } from '@testing-library/react';
+import { Checkbox } from '../components/Checkbox';
+
+
+beforeEach(cleanup);
+
+jest.mock('../firebase.js', ()=>({
+	firebase : {
+		firestore : jest.fn(()=>({
+			collection : jest.fn(()=>({
+				doc : jest.fn(()=>({
+					update : jest.fn(),
+				})),
+			})),
+		})),
+	},
+}));
+
+
+describe('</Checkbox', ()=> {
+	describe('success', ()=>{
+		it('renders the task checkbox', () => {
+			const { queryByTestId } = render(
+				<Checkbox id ="1" taskDesc="Getting my feeting wet in React" />
+			);
+
+			expect( queryByTestId('checkbox-action')).toBeTruthy();
+			//debug();
+		});
+
+		it('renders the task checkbox and accept a click', ()=>{
+			const { queryByTestId } = render(
+				<Checkbox id="1" taskDesc="Getting my feeting wet in React" />
+			);
+
+			expect(queryByTestId('checkbox-action')).toBeTruthy();
+			fireEvent.click(queryByTestId('checkbox-action'))
+		});
+		
+		it('renders the task checkbox and accept a keyDown', ()=>{
+			const { queryByTestId } = render(
+				<Checkbox id="1" taskDesc="Getting my feeting wet in React" />
+			);
+
+			expect(queryByTestId('checkbox-action')).toBeTruthy();
+			fireEvent.keyDown(queryByTestId('checkbox-action'))
+		});
+	})
+});
