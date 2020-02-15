6ce9a02111bbc392e13626c10005ef9c6c814a28
Ray Kudjie <rkudjie@gmail.com>
2020-02-15 14:55:13 +0000

    Move key to .env file

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@ import firebase from "firebase/app";
 import "firebase/firestore";
 
 const firebaseConfig = firebase.initializeApp({
-	apiKey: "AIzaSyCZv5iLRveHdFR7bVactjKEihz9wDDiwO4",
-	authDomain: "todoist-clone-5c952.firebaseapp.com",
-	databaseURL: "https://todoist-clone-5c952.firebaseio.com",
-	projectId: "todoist-clone-5c952",
-	storageBucket: "todoist-clone-5c952.appspot.com",
-	messagingSenderId: "187962965480",
-	appId: "1:187962965480:web:4dc7971de4b7e4fbf9385d"
-})
+	apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+	authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+	databaseURL: process.env.REACT_APP_DATABASE_URL,
+	projectId: process.env.REACT_APP_PROJECT_ID,
+	storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+	messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+	appId: process.env.REACT_APP_APP_ID
+});
 
 export { firebaseConfig as firebase }
